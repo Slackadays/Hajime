922399e783faf0b26660ad4652d0f657b8dfb6b8
Slackadays <69403549+Slackadays@users.noreply.github.com>
2020-08-09 17:21:11 -0400

    Add files via upload

---
+++ hajime3.cpp
@@ -0,0 +1,168 @@
+//Hajime version 1.0
+//(c) 2020 Jackson Huff.
+//Released under the GPLv3 license
+#include <iostream>
+#include <cstring>
+#include <experimental/filesystem>
+#include <fstream>
+#include "server3.h"
+
+using std::cout;
+using std::endl;
+namespace fs = std::experimental::filesystem::v1;
+
+string confFile; // = "start.conf";
+string sconfFile = "hajime.conf";
+string sysdService = ""; // = "/etc/systemd/system/start.service"; //systemd service file location
+string logFile;
+
+void makeConfig();
+void makeSysd();
+void readSettings();
+
+//argNum is the number of arguments from the command line, *args[] is the arguments themselves
+int main(int argNum, char *args[]) {
+	if (fs::is_regular_file(sconfFile)) {
+		readSettings();
+		if (logFile == "") {
+			cout << "No log file to be made!" << endl;
+		}
+
+	} else {
+		cout << "Config file doesn't exist!" << endl;
+	}
+	int i = 0;
+	while (i < argNum) {
+		
+		//allow the user to choose a file preceded by -f, strcmp() compares a C pointer and a primitive type
+		if (strcmp(args[i], "-f") == 0) {
+			confFile = args[(i + 1)];
+		}
+		
+		if (strcmp(args[i], "-h") == 0 || strcmp(args[i], "--help") == 0) {
+			cout << "Hajime is a high-performance startup script designed to start a Minecraft server from an external device. Usage: \n" << 
+			args[0] << " [-f configuration-file] [-h] [-I] [-S] \n" <<
+			"-f is used in conjunction with a custom config file. A plain filename is interpreted as the same directory the script is located in, so use a / to specify otherwise." << endl;
+			return 0;
+		}
+		
+		if (strcmp(args[i], "-I") == 0) {
+			makeConfig();
+			return 0;
+		}
+		
+		if (strcmp(args[i], "-S") == 0) {
+			makeSysd();
+			return 0;
+		}
+		
+		i++;
+	}
+	Server one;
+	one.startServer(confFile);
+	return 0;
+}
+
+void makeConfig() {
+	cout << "Installing config file..." << endl;
+	if (fs::is_regular_file(confFile) == true){
+		cout << "The file is already here! To make a new one, delete the existing file." << endl;
+	} else {
+		ofstream outConf(confFile);
+		outConf << "File=SERVER-FILE"<< endl << "Path=PATH-TO-DEVICE" << endl << "Command=SERVER-EXECUTION-COMMAND" << endl << "Debug=1" << endl << "Device=DEVICE" << endl;
+		outConf << "#" << endl << "This is the comment section. Anything after the # is a comment. \n The first line is the file of the server that needs to be executed. The second line is the path that leads to the home directory of the external device. The third line is the command that needs to be executed in order to start the server. The fourth line is the debug setting. 0 means most output is disabled. 1 prevents most looped outputs. 2 enables all outputs. I recommend 1, but switch to 2 if there\'s a problem somewhere." << endl;
+		cout << "The config file (" << confFile << ") has been created and is now ready for your settings." << endl;
+		outConf.close();
+	}
+	if (fs::is_regular_file(sconfFile)) {
+		cout << "There is a config file here!" << endl;
+	} else {
+		ofstream outsConf(sconfFile);
+		outsConf <<	"DefaultServerConf=start.conf" 
+		<< endl << "SystemdLocation=/etc/systemd/system/start.service"
+		<< endl << "Logfile="
+		<< endl << "#"
+		<< endl;
+		cout << "Config file made!" << endl;
+		outsConf.close();
+	}
+}
+
+void readSettings() {
+	
+	//conjure up a file stream
+	std::fstream sconf;
+	//configuration file open for reading
+	sconf.open(sconfFile, std::fstream::in);
+
+	int iter = 0;
+	int lineNum = 0;
+	string var[4], param[4], line;
+    string finished = "";
+	//checks if there's stuff left to read
+	while (sconf.good() && lineNum < 3) { //linenum < 6 because otherwise, we get a segmentation fault
+		getline(sconf, line); //get a line and save it to line
+		
+		if (line == ""){
+			throw "Whoops! The config file doesn't have anything in it.";
+		}
+		//if we've reachd the end of the config section (#) then get out of the loop!
+		if (line[iter] == '#') {
+					break;
+		}
+		
+		param[lineNum] = "";
+		//skips past anything that isn't in a quote
+		//single quotes mean a char, and escape the double quote with a backslash
+		while (line[iter] != '=') {
+			param[lineNum] = param[lineNum] + line[iter];
+			iter++;
+		}
+		//the current position is that of a quote, so increment it 1
+		iter++;
+		//append the finished product
+		while ((uint)iter < line.length()) {
+			finished = finished + line[iter];
+			iter++;
+		}
+
+		//make the var[] what the finished product is
+		var[lineNum] = finished;
+		//reset for the next loop
+		iter = 0;
+		finished = "";
+		if (param[lineNum] == "DefaultServerConf") {confFile = var[lineNum];}
+		if (param[lineNum] == "Logfile") {logFile = var[lineNum];}
+		if (param[lineNum] == "SystemdLocation") {sysdService = var[lineNum];}
+		//prep var[] for the next line
+		lineNum++;
+	}
+	
+	//get rid of the file in memory
+	sconf.close();
+}
+
+void makeSysd() {
+	if (fs::is_regular_file(sconfFile) == true && sysdService == "") {
+		readSettings();
+	}
+	if (fs::is_regular_file(sconfFile) == false) {
+		cout << "Sorry! Please run Hajime with the -I flag to make a config file first." << endl;
+		return;
+	}
+	if (fs::is_directory("/etc/systemd") == true && fs::is_regular_file(sysdService) == true) {
+		cout << "The systemd service is already here!" << endl;
+	}
+	if (fs::is_directory("/etc/systemd") == true && fs::is_regular_file(sysdService) == false) {
+		cout << "Making systemd service..." << endl;
+		ofstream service(sysdService);
+		service << "[Unit]" << endl << "Description=Starts Hajime" << endl << endl << "[Service]\nType=simple\nWorkingDirectory=" << fs::current_path().string() << "\nExecStart=sudo " << fs::current_path().string()  << "/hajime\n\n[Install]\nWantedBy=multi-user.target";
+		service.close();
+	}
+	if (fs::is_directory("/etc/systemd") == false) {
+		cout << "Looks like you don't have systemd." << endl;
+	}
+}
+// compile command
+// sudo g++ -o hajime hajime.cpp -lstdc++fs
+// filesystem library is experimentral in g++ 8, so an extra thing is needed to include it
---
+++ server3.h
@@ -0,0 +1,306 @@
+//Hajime class and function files
+
+#include <experimental/filesystem>
+#include <stdlib.h>
+#include <fstream>
+#include <sys/mount.h>
+#include <cstring>
+#include <errno.h>
+#include <iostream>
+#include <string>
+
+#ifdef _WIN32
+	#include <Windows.h> //Windows library
+#else
+	#include <unistd.h> //Linux version of the library
+#endif
+
+using namespace std;
+namespace fs = std::experimental::filesystem::v1;
+
+class Server {
+	bool hasOutput = false;
+	bool hasOutputUSB = false; //if one variable is used, then the debug action will interfere wihh the USB mount function
+	bool hasMounted = false;
+	int debug = 2; //set to 0 to get rid of most messages, 1 for 1 message per action, 2 for all messages available
+	int systemi = 0;
+	int maxPID = 0;
+
+	const string systems[7] = {"ext2", "ext3", "ext4", "vfat", "msdos", "f2fs", "fuseblk"};
+	
+	void mountDrive();
+	void makeDir();
+	void startProgram();
+	void readSettings(string confFile);
+	void startServer();
+	void printMessage(string message, int = 1);
+
+	string file, path, command, confFile, device;
+	
+	public:
+		Server();
+		bool isRunning = false;
+		void startServer(string confFile);
+		int getPID();
+};
+
+Server::Server() {
+string oldPath = fs::current_path();
+int i = 0; //start from beginning
+fs::current_path("/proc"); //proc holds all the processes
+while (i < 20000) { //20000 is the highest likely process PID
+	if (fs::exists(to_string(i))){ //i needs to be a string to use with the fs functions
+			maxPID = i;
+	}
+	i++; //go up 1 PID
+}
+fs::current_path(oldPath);
+}
+
+void Server::startServer(string confFile) {
+	try {
+		if (fs::is_regular_file(confFile)) {
+			printMessage("Reading settings...", 1);
+			readSettings(confFile);
+		} else {
+			cout << "The server's config file doesn't exist" << endl;
+			return;
+		}
+		
+		switch(debug) {
+			
+		case 0:
+			break;
+			
+		case 1:
+			cout << "The file is: " << file << endl;
+			cout << "The path is: " << path << endl;
+			cout << "Command: " << command << endl;
+			cout << "Debug value: " << debug << endl;
+			cout << "Device: " << device << endl;
+			break;
+			
+		case 2: 
+			cout << "The file is: " << file << endl;
+			cout << "The path is: " << path << endl;
+			cout << "Command: " << command << endl;
+			cout << "Debug value: " << debug << endl;
+			cout << "Device: " << device << endl;
+			break;
+			
+		}
+		
+		while(true) {
+			if (getPID() != 0) { //getPID looks for a particular keyword in /proc/PID/cmdline that signals the presence of a server
+				sleep(3);
+				printMessage("Program is running!", 1);
+				isRunning = true;
+				hasMounted = true;
+				} else {
+					isRunning = false;
+				}
+				
+			fs::current_path(path);
+
+			//checks if we're in the right place and if the server file is there
+			if (fs::current_path() == path && fs::is_regular_file(file) && isRunning == false) {
+				startProgram();
+			}
+
+			sleep(2);
+			
+			//if the desired path doesn't exist, make it
+			if (!fs::is_directory(path)) {
+				makeDir();
+			}
+			
+			fs::current_path(path);
+			
+			if (!hasMounted) {
+				mountDrive();
+			}
+		}
+	} catch(string mes){
+		printMessage(mes, 1);
+	} catch(...) { //error handling
+		printMessage("Whoops! An error occurred.", 1);
+	}
+}
+
+void Server::startProgram() {
+	if (!isRunning) {
+		
+		printMessage("Starting program!", 1);
+		
+		fs::current_path(path);
+
+		//session.lock will be there if the server didn't shut down properly
+		fs::remove("world/session.lock");
+
+		//system() is a C command too
+		system(command.c_str()); //execute the command
+		
+		sleep(1);
+		
+		if (getPID() != 0) { //check for the PID of the program we just started
+			isRunning = true; //isRunning disables a lot of checks
+			hasMounted = true;
+		}
+	}
+}
+
+void Server::makeDir() {
+	printMessage("No directory!", 2);
+	if (!fs::create_directory(path)) {
+		cout << "Error creating directory!" << endl;
+	}
+}
+
+void Server::mountDrive() {
+	printMessage("Trying to mount.", 1);
+	//sda1 is the first external mass storage device
+	if (!fs::is_empty(path)) { //if there are files, then we don't want to mount there
+			printMessage("There are files in the path", 2);
+			return;
+	} else {
+		string error;
+	if (mount(device.c_str(), path.c_str(), systems[systemi].c_str(), 0, "") == 0) { //brute-forces every possible filesystem because mount() depends on it being the right one
+		printMessage("Device mounted!", 1);
+		hasMounted = true;
+		systemi = 0; //reset in case it needs to mount again
+	} else {
+		int errsv = errno; //errno is the POSIX error code
+		if (systemi == 6) {
+			switch (errsv) {
+			case 1 : error = "Not permitted. Is the device correct?"; break;
+			case 2 : error = "No such file or directory."; break;
+			case 13: error = "Permission denied. Is Hajime being run under root?"; break;
+			case 5 : error = "Input/output error. Is the drive OK?"; break;
+			case 12: error = "Not enough memory. Is there a shortage of it?"; break;
+			case 11: error = "Resource unavailable."; break;
+			case 14: error = "Bad address."; break;
+			case 15: error = "Not a block device. Did you make sure you're mounting a mass storage device?"; break;
+			case 16: error = "Busy. Is the device being accessed right now?"; break;
+			case 21: error = "It's a directory. Did you make sure you're mounting a mass stoage device?"; break;
+			case 22: error = "Bad arguments. Is the configuration set correctly?"; break;
+			case 19: error = "Unknown device. Is the filesystem supported?"; break;
+			default: error = "Unknown error.";
+			}
+			if (hasOutputUSB == false){
+				printMessage("An error occurred, but the script will keep trying to mount. Error: " + error, 1);
+				hasOutputUSB = true;
+				systemi = 0;
+			}
+			printMessage("Error code: " + to_string(errsv), 2);
+			}
+		}
+		if (systemi < 6) {
+			cout << "Trying " << systems[systemi] << " filesystem" << endl;
+			systemi++; //increment the filesystem
+		}
+	}
+}
+
+void Server::readSettings(string confFile) {
+	
+	//conjure up a file stream
+	std::fstream conf;
+
+	//configuration file open for reading
+	conf.open(confFile, std::fstream::in);
+
+	int iter = 0;
+	int lineNum = 0;
+	string var[6], param[6], line;
+    string finished = "";
+	//checks if there's stuff left to read
+	while (conf.good() && lineNum < 6) { //linenum < 6 because otherwise, we get a segmentation fault
+		getline(conf, line); //get a line and save it to line
+		
+		if (line == ""){
+			throw "Whoops! The config file doesn't have anything in it.";
+		}
+		//if we've reachd the end of the config section (#) then get out of the loop!
+		if (line[iter] == '#') {
+					break;
+		}
+		param[lineNum] = "";
+		//skips past anything that isn't in a quote
+		//single quotes mean a char, and escape the double quote with a backslash
+		while (line[iter] != '=') {
+			param[lineNum] = param[lineNum] + line[iter];
+			iter++;
+		}
+		//the current position is that of a quote, so increment it 1
+		iter++;
+		//append the finished product
+		while ((uint)iter < line.length()) {
+			finished = finished + line[iter];
+			iter++;
+		}
+
+		//make the var[] what the finished product is
+		var[lineNum] = finished;
+		//reset for the next loop
+		iter = 0;
+		finished = "";
+		if (param[lineNum] == "File") {file = var[lineNum];}
+		if (param[lineNum] == "Path") {path = var[lineNum];}
+		if (param[lineNum] == "Command") {command = var[lineNum];}
+		//stoi() converts the string result into an int
+		if (param[lineNum] == "Debug") {debug = stoi(var[lineNum]);}
+		if (param[lineNum] == "Device") {device = var[lineNum];}
+		//prep var[] for the next line
+		lineNum++;
+	}
+	
+	if (device == "") {
+		printMessage("No device requested: No mounting this time!", 1);
+		hasMounted = true;
+	}
+
+	//get rid of the file in memory
+	conf.close();
+}
+
+int Server::getPID() {
+string oldPath = fs::current_path();
+int i = maxPID; //start from beginning
+fs::current_path("/proc"); //proc holds all the processes
+while (i < 20000) { //20000 is the highest likely process PID
+	if (fs::exists(to_string(i))){ //i needs to be a string to use with the fs functions
+			fstream file; //create a file object
+			file.open(to_string(i) + "/cmdline", ios::in); //open the file of /proc/PID/cmdline for reading
+			string str = ""; //reset string
+			getline(file, str); //read cmdline (it is only 1 line)
+			if (str.length() > 0){ //if a cmdline is not used, there will be nothing
+				if (str.find("SCREEN") != string::npos){ //look for a keyword in cmdline, string::npos is a special value (-1) that needs to be used
+					file.close(); //erase from memory
+					fs::current_path(oldPath);
+					return i; //return the PID of the known good process
+				
+				}
+			}
+			file.close(); //erase the file from memory
+	}
+	i++; //go up 1 PID
+}
+fs::current_path(oldPath);
+return 0; //0 is the signal for "doesn't exist"
+}
+
+void Server::printMessage(string message, int v) { //v = verbosity
+	switch (debug) {
+		case 0: 
+			break;
+		case 1:
+			if (v == 1) {cout << message << endl; break;} 
+			if (v == 2) {break;}
+		case 2:
+			if (v == 1 || v == 2) {cout << message << endl; break;}
+		default: //if debug isn't set, we don't want to miss anything
+			cout << message << endl;
+	}
+}
+
+
