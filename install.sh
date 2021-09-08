8baa321054895fcb89f78e3938bffeb567a8fa0f
Slackadays <69403549+Slackadays@users.noreply.github.com>
2021-09-08 13:23:30 -0400

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo "\e[1mInstalling g++ and git...\e[0m"
-apt update && apt install -y g++-10 git
+apt update && apt install -y g++-10 git #g++-10 or the latest version of g++
 echo "\e[1mDownloading...\e[0m"
 git clone https://github.com/Slackadays/Hajime
 echo "\e[1mCompiling...\e[0m"
 cd Hajime/source
 g++ -Ofast -std=c++20 -o hajime hajime.cpp
 echo "\e[1mCleaning up...\e[0m"
-mv hajime ../../
-rm -r ../../Hajime
+mv hajime ../../ #move the binary to the original folder where the script was started
+rm -r ../../Hajime #remove the cloned directory
 
