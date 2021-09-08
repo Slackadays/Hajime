b99e60a8b9f4d6f6fbebcf66c72fafe37c67a492
Slackadays <69403549+Slackadays@users.noreply.github.com>
2021-09-07 21:36:20 -0400

    Clean up keepEndlines and add "warning" category

--- source/output.h
+++ source/output.h
@@ -14,7 +14,7 @@ class Output {
 	bool logToFile = false;
 	string logFilename;
 	ofstream fileObj;
-	string removeEndlines(string input);
+	string removeEndlines(string input, bool keepEndlines);
 	string addColorsByType(string data, string type);
 	public:
 		void out(string data, string type, bool keepEndlines);
@@ -30,17 +30,9 @@ void Output::init(string file) {
 
 void Output::out(string data, string type = "none", bool keepEndlines = false){
 	if (!logToFile){
-		if (!keepEndlines){
-                        cout << Output::addColorsByType(Output::removeEndlines(data), type) << endl;
-                } else {
-                        cout << Output::addColorsByType(data, type) << endl;
-                }
+                cout << Output::addColorsByType(Output::removeEndlines(data, keepEndlines), type) << endl;
 	} else {
-		if (!keepEndlines){
-			fileObj << Output::addColorsByType(Output::removeEndlines(data), type) << endl;
-		} else {
-			fileObj << Output::addColorsByType(data, type) << endl;
-		}
+		fileObj << Output::addColorsByType(Output::removeEndlines(data, keepEndlines), type) << endl;
 	}
 }
 
@@ -49,18 +41,21 @@ void Output::end(){
 	logToFile = false;
 }
 
-string Output::removeEndlines(string input){
-	for (int i = 0; i <= input.length(); i++){ //removes all \n's from the string
-		while (input[i] == '\n'){ //loops a position because when the character is removed, the next one shifts into place
-			input[i] = '\0'; // \0 is the null character
+string Output::removeEndlines(string input = "", bool keepEndlines = false){
+	if (!keepEndlines){ 
+		for (int i = 0; i <= input.length(); i++){ //removes all \n's from the string
+			while (input[i] == '\n'){ //loops a position because when the character is removed, the next one shifts into place
+				input[i] = '\0'; // \0 is the null character
+			}
 		}
 	}
 	return input;
 }
 
-string Output::addColorsByType(string data, string type){
-	if (type == "none"){return data;}
-	if (type == "info"){return "\e[1;46m[Info]\e[1;0m " + data;}
-	if (type == "error"){return "\e[1;41m\e[1;33m[Error]\e[1;0m " + data;}
-	return data;
+string Output::addColorsByType(string input = "", string type = "none"){
+	if (type == "none"){return input;} //"none" is if you want to preserve input
+	if (type == "info"){return "\e[1;46m[Info]\e[1;0m " + input;} //cyan background
+	if (type == "error"){return "\e[1;41m\e[1;33m[Error]\e[1;0m " + input;} //red background, yellow text
+	if (type == "warning"){return "\e[1;33m[Warning]\e[1;0m " + input;} //yellow text
+	return input;
 }
