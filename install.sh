9d8d0e1e5ab54f11c1fbdc3727f7191b4df66afd
Slackadays <69403549+Slackadays@users.noreply.github.com>
2021-09-08 08:40:25 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ apt update && apt install -y g++-10 git
 echo "\e[1mDownloading...\e[0m"
 git clone https://github.com/Slackadays/Hajime
 echo "\e[1mCompiling...\e[0m"
-g++ -Ofast -std=c++20 -o hajime Hajime/source/hajime.cpp
+cd Hajime/source
+g++ -Ofast -std=c++20 -o hajime hajime.cpp
 echo "\e[1mCleaning up...\e[0m"
-rm -r Hajime
+mv hajime ../../
+rm -r ../../Hajime
+
