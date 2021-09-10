8f559acd2079b0e9afd1868c3b21137534410548
Slackadays <69403549+Slackadays@users.noreply.github.com>
2021-09-10 16:33:44 -0400

    Update g++ command

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 echo "\e[1mInstalling g++ and git...\e[0m"
-apt update && apt install -y g++ git #g++-10 or the latest version of g++
+apt update && apt install -y g++ git #g++-10 or the otherwise latest version of g++ that supports c++17
 echo "\e[1mDownloading...\e[0m"
 git clone https://github.com/Slackadays/Hajime
 echo "\e[1mCompiling...\e[0m"
 cd Hajime/source
-g++ -Ofast -std=c++17 -o hajime hajime.cpp
+g++ -Ofast -std=c++17 -o hajime hajime.cpp -lstdc++fs #lstdc++fs enables filsystem library in older installations
 echo "\e[1mCleaning up...\e[0m"
 mv hajime ../../ #move the binary to the original folder where the script was started
 rm -r ../../Hajime #remove the cloned directory
-
