7814472f18b80e87463b105e58e3be1d642971d8
Slackadays <69403549+Slackadays@users.noreply.github.com>
2021-09-17 12:20:58 -0400

    Compact code, eliminate commenting bug

--- source/getvarsfromfile.h
+++ source/getvarsfromfile.h
@@ -10,36 +10,23 @@ using std::vector;
 vector<string> getVarsFromFile(string filename, vector<string> inputVars);
 
 vector<string> getVarsFromFile(string filename, vector<string> inputVars) {
-        std::fstream file;
-        file.open(filename, std::fstream::in); //configuration file open for reading
-        vector <string> outputVars;
+        std::fstream file(filename, std::fstream::in); //configuration file open for reading
+	vector <string> outputVars;
         string line= "", temp1 = "", temp2 = "";
-        bool skipLine = false;
  	for (unsigned int i = 0, lineNum = 0; file.good() && !file.eof(); lineNum++, i = 0, temp1 = "", temp2 = "") {
                 getline(file, line); //get a line from file and save it to line
- 		if (line[i] == '\n' || line[i] == '\0' || line[i] == '#') {
-                        skipLine = true;
-                }
-		if (!skipLine) {
-                	for (;line[i] != '='; i++) { //skips past anything that isn't in a quote
+		if ((line[i] != '\0') && (line[i] != '#')) {
+                	for (;(line[i] != '=') && (line[i] != '#'); i++) { //skips past anything that isn't in a quote
                 	        temp1 += line[i];
-				if (line[i] == '#') {
-					break;
-				}
                 	}
-                	i++; //the current position is that of a quote, so increment it 1
-                	for (;i < line.length(); i++) {
-                	        temp2 += line[i]; //append the finished product
-				if (line[i] == '#') {
-                                        break;
-                                }
+                	for (i++;(i < line.length()) && (line[i] != '#'); i++) { //increment i by 1 to skip the =
+                	        temp2 += line[i];
                 	}
-			for (vector<string>::iterator paramIterator = inputVars.begin(); paramIterator != inputVars.end(); ++paramIterator) {
-				if (temp1 == *paramIterator) {
+			for (const auto &iteratorVal : inputVars) {
+				if (temp1 == iteratorVal) {
 					outputVars.push_back(temp2);
 				}
 			}
-        
 		}
 	}
         file.close();  //get rid of the file in memory
