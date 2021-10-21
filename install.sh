3a26ea34e9818c5b38e7023b91b3f49445e7a661
Slackadays <69403549+Slackadays@users.noreply.github.com>
2021-10-20 22:22:59 -0400

    Add OpenBSD compat + fix compile error

--- install.sh
+++ install.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 echo "\e[1mInstalling g++ and git...\e[0m"
-sudo apt update && sudo apt install -y g++ git #g++-10 or the otherwise latest version of g++ that supports c++17
+if [ "$OSTYPE" = "FreeBSD" ]
+then
+        echo "FreeBSD detected"
+        if [ "$USER" != "root" ]
+        then
+                echo "You may need to be the root user to install Hajime"
+        fi
+        pkg update
+        pkg install gcc10 git
+        pkg upgrade
+else
+        echo "Linux detected"
+        sudo apt update && sudo apt install -y g++ git #g++-10 or the otherwise latest version of g++ that supports c++17
+        sudo apt upgrade
+fi
 echo "\e[1mDownloading...\e[0m"
 git clone https://github.com/Slackadays/Hajime
 echo "\e[1mCompiling...\e[0m"
 echo "\e[1mThis may take from a few seconds to a few minutes depending on your system speed.\e[0m"
 cd Hajime/source
-g++ -Ofast -march=native -static -std=c++17 -o hajime hajime.cpp -lstdc++fs #lstdc++fs enables filsystem library in older installations
+g++ -Ofast -march=native -static -std=c++17 -o hajime hajime.cpp -lstdc++fs -pthread #lstdc++fs enables filsystem library in older installations
 echo "\e[1mCleaning up...\e[0m"
 chown -R $USER ../../Hajime/.git ../../Hajime/source/hajime #change perms for certain misbehaving files that come with git
 mv hajime ../../ #move the binary to the original folder where the script was started
