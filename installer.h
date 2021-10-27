6fb6364c250475003b4b3106762f604b854e6363
Slackadays <69403549+Slackadays@users.noreply.github.com>
2021-10-27 14:29:50 -0400

    Actually fix W7 compat

---
+++ installer.h
@@ -0,0 +1,111 @@
+#include <fstream>
+#include <filesystem>
+#include <cstring>
+#include <string>
+
+#if defined(_win64) || defined (_WIN32)
+#include <Windows.h>
+#include <shlobj_core.h>
+#else
+#include <unistd.h>
+#endif
+
+using std::cout;
+using std::endl;
+
+#include "output.h"
+
+class Installer {
+	std::shared_ptr<Output> logObj;
+	void installNewServerConfigFile(string fileLocation);
+	public:
+		void installStartupService(string sysService);
+		void installDefaultHajConfFile(string fileLocation);
+		void installDefaultServerConfFile(string conf);
+		void installDefaultServersFile(string serversFile);
+		Installer(std::shared_ptr<Output> log);
+};
+
+Installer::Installer(std::shared_ptr<Output> log) {
+	logObj = log;
+}
+
+void Installer::installDefaultServerConfFile(string conf) {
+	logObj->out("Installing default server config file...", Info);
+	if (fs::is_regular_file(conf)){
+		logObj->out("The file is already here! To make a new one, delete the existing file.", Warning);
+		logObj->out("Would you like to create a new configuration file anyway?", Info, 0, 0); // don't keep endlines, don't add endline
+		if (logObj->getYN()) {
+			logObj->out("Installing a new server config file...", Info);
+			Installer::installNewServerConfigFile(conf);
+		}
+	} else {
+		Installer::installNewServerConfigFile(conf);
+	}
+}
+
+void Installer::installDefaultHajConfFile(string fileLocation = "(none)") {
+	logObj->out("Installing default Hajime config file " + fileLocation + "...", Info);
+	logObj->out("Checking for existing file...", Info);
+	if (fs::is_regular_file(fileLocation)) {
+		logObj->out("Hajime config file already present!", Warning);
+	} else {
+		ofstream outConf(fileLocation);
+		outConf << "serversfile=servers.conf" << endl;
+		outConf << "logfile=" << endl;
+		outConf << "systemdlocation=/etc/systemd/system/hajime.service" << endl;
+		outConf.close();
+		logObj->out("Hajime config file made!", Info);
+	}
+}
+
+void Installer::installNewServerConfigFile(string fileLocation) {
+	ofstream outConf(fileLocation);
+	outConf << "file=server.jar" << endl << "path=PATH" << endl << "command=COMMAND" << endl << "flags=FLAGS" << endl << "debug=1" << endl << "device=" << endl;
+	outConf << "# Anything after a # is a comment." << endl;
+	cout << "The config file (" << fileLocation << ") has been created and is now ready for your settings." << endl;
+	outConf.close();
+}
+
+
+void Installer::installStartupService(string sysService) {
+	#if defined(_WIN64) || defined (_WIN32)
+	logObj->out("Installing Windows startup service", Info);
+	string command = "schtasks.exe /create /sc ONLOGON /tn Hajime /tr " + fs::current_path().string() + "\\Hajime.exe";
+	cout << command << endl;
+	int result = system(command.c_str());
+	if (!IsUserAnAdmin()) {
+		logObj->out("You need to run Hajime as the administrator to install a startup service.", Error);
+		logObj->out("Tip: Right click the terminal icon and then click \"Run as administrator\"", Info);
+	}
+	#else
+	if (getuid()) {logObj->out("You need to be the root user to install a systemd service", Error);}
+	if (fs::is_directory("/etc/systemd") && fs::is_regular_file(sysService)) {
+		logObj->out("Found an existing systemd service", Warning);
+	}
+	if (fs::is_directory("/etc/systemd") && !fs::is_regular_file(sysService)) {
+		logObj->out("Making systemd service...", Info);
+		ofstream service(sysService);
+		service << "[Unit]" << endl << "Description=Starts Hajime" << endl;
+		service << endl << "[Service]\nType=simple\nWorkingDirectory=" << fs::current_path().string() << "\nExecStart=" << fs::current_path().string()  << "/hajime\n\n[Install]\nWantedBy=multi-user.target";
+		service.close();
+	}
+	if (!fs::is_directory("/etc/systemd")) {
+		logObj->out("Looks like there is no systemd; use another installation option instead.", Error);
+	}
+	#endif
+}
+
+
+void Installer::installDefaultServersFile(string serversFile) {
+	logObj->out("Installing default servers file...", Info);
+	logObj->out("Checking for existing file...", Info);
+	if (fs::is_regular_file(serversFile)) {
+		logObj->out("Servers file already present!", Warning);
+	} else {
+		ofstream outConf(serversFile);
+		outConf << "server0.conf" << endl;
+		outConf.close();
+		logObj->out("Servers file made!", Info);
+	}
+}
---
+++ output.h
@@ -0,0 +1,99 @@
+#pragma once //this guards against g++ error "redefinition of class Output"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <algorithm>
+#include <regex>
+
+using std::string;
+using std::ofstream;
+
+enum Type {None, Info, Error, Warning, Debug};
+
+class Output {
+	std::mutex outMutex;
+	std::mutex endMutex;
+	std::thread::id main_thread = std::this_thread::get_id();
+	bool logToFile = false;
+	bool debug = true;
+	string logFilename;
+	ofstream fileObj;
+	string removeEndlines(string input, bool keepEndlines);
+	string addPrefixByType(string data, Type type);
+	public:
+		void out(string data, Type type, bool keepEndlines, bool endLineAtEnd);
+		bool getYN(string prompt);
+		void init(string file, bool debugOrNot);
+		void end();
+		bool noColors = false;
+};
+
+void Output::init(string file, bool debugOrNot = true) {
+	logToFile = true;
+	logFilename = file;
+	debug = debugOrNot;
+	fileObj.open(logFilename, std::ios::app); //appends to a current file and creates it if needed
+}
+
+void Output::out(string data, Type type = None, bool keepEndlines = false, bool endLineAtEnd = true) {
+	if (!debug && type == Debug) {
+		return;
+	}
+	string outputString = Output::addPrefixByType(Output::removeEndlines(data, keepEndlines), type);
+	if (noColors) {
+		outputString = std::regex_replace(outputString, std::regex("\\\033\\[\\d*m", std::regex_constants::optimize), ""); //I hate this
+	}
+	if (!logToFile) {
+		std::lock_guard<std::mutex> lock(outMutex);
+		std::cout << outputString;
+		if (endLineAtEnd) {
+			std::cout << std::endl;
+		}
+	} else {
+		std::lock_guard<std::mutex> lock(outMutex);
+		fileObj << outputString << std::endl;
+	}
+}
+
+bool Output::getYN(string prompt = "[y/n]") {
+	string response = "";
+	if (prompt != "") {
+		prompt = " " + prompt + " ";
+	}
+	this->out("\033[1m" + prompt, None, 0, 0);
+	std::cin >> response;
+	this->out("\033[0m", None, 0, 0);
+	if (response == "y" || response == "Y" || response == "yes" || response == "Yes" || response == "YES" || response == "YEs") {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+void Output::end(){
+	std::lock_guard<std::mutex> lock(endMutex);
+	fileObj.close();
+	logToFile = false;
+}
+
+string Output::removeEndlines(string input, bool keepEndlines = false){
+	if (!keepEndlines){
+		input.erase(std::remove(input.begin(), input.end(), '\n'), input.end());
+	}
+	return input;
+}
+
+string Output::addPrefixByType(string input = "", Type type = None){
+	string prefix = "";
+	if (type == None){return input;} //None is if you want to preserve input
+	if (type == Info){prefix = text.prefixInfo;} //cyan background
+	if (type == Error){prefix = text.prefixError;} //red background, yellow text
+	if (type == Warning){prefix = text.prefixWarning;} //yellow text
+	if ((type == Debug) && debug){prefix = text.prefixDebug;} //magenta background
+	if (main_thread == std::this_thread::get_id()) {prefix += "| Main ]\033[0m ";} else {prefix += "| Worker ]\033[0m ";}
+	return (prefix + input);
+}
