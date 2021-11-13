ee0aea51c74b35c8bc64464a7fbf2050bdbcc8a3
Slackadays <69403549+Slackadays@users.noreply.github.com>
2021-11-13 10:07:27 -0500

    Add initial sysvinit support

--- source/installer.cpp
+++ source/installer.cpp
@@ -63,7 +63,6 @@ void Installer::installNewServerConfigFile(string fileLocation) {
 	outConf.close();
 }
 
-
 void Installer::installStartupService(string sysService) {
 	#if defined(_WIN64) || defined (_WIN32)
 	logObj->out("Installing Windows startup service", Info);
@@ -75,21 +74,82 @@ void Installer::installStartupService(string sysService) {
 		logObj->out("Tip: Right click the terminal icon and then click \"Run as administrator\"", Info);
 	}
 	#else
-	if (getuid()) {logObj->out(text.errorSystemdRoot, Error);}
-	if (fs::is_directory("/etc/systemd") && fs::is_regular_file(sysService)) {
-		logObj->out("Found an existing systemd service", Warning);
+	if (fs::is_directory("/etc/init.d") && !fs::is_regular_file("/lib/systemd/systemd")) {
+		logObj->out("Installing sysVinit service", Info);
+		ofstream service("/etc/init.d/hajime.sh");
+		service << "#!/bin/sh\n"
+		"### BEGIN INIT INFO\n"
+		"# Provides: 		hajime\n"
+		"# Required-Start:\n"
+		"# Required-Stop:\n"
+		"# Default-Start:	3\n"
+		"# Default-Stop:\n"
+		"# Short-Description:	Starts Hajime\n"
+		"# Description:		SysVinit service to start the Hajime startup script.\n"
+		"### END INIT INFO\n"
+		"NAME=\"hajime\"\n"
+		"PATH=\"/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin\"\n"
+		"APPDIR=\"/root/Downloads/hajime/source\"\n"
+		"APPBIN=\"/root/Downloads/hajime/source/hajime\"\n"
+		"APPARGS=\"-h\"\n"
+		"USER=\"vm\"\n"
+		"GROUP=\"vm\"\n"
+		"set -e\n"
+		". /lib/lsb/init-functions\n"
+		"start() {\n"
+		"	printf \"Starting Hajime... \"\n"
+		"	start-stop-daemon --start --chuid \"$USER:$GROUP\" --background --make-pidfile --pidfile /var/run/$NAME.pid --chdir \"$APPDIR\" --exec \"$APPBIN\" -- $APPARGS || true\n"
+		"	printf \"done!\n\"\n"
+		"}\n"
+		"stop() {\n"
+		"	printf \"Stopping Hajime!\n\"\n"
+		"	[ -z `cat /var/run/$NAME.pid 2>/dev/null` ] || \\\n"
+		"	kill -9 $(cat /var/run/$NAME.pid)\n"
+		"	[ -z `cat /var/run/$NAME.pid 2>/dev/null` ] || rm /var/run/$NAME.pid\n"
+		"	printf \"Stopped Hajime!\n\"\n"
+		"}\n"
+		"status() {\n"
+		"	status_of_proc -p /var/run/$NAME.pid \"\" $NAME && exit 0 || exit $?\n"
+		"}\n"
+		"case \"$1\" in\n"
+		"	start)\n"
+		"		start\n"
+		"		;;\n"
+		"	stop)\n"
+		"		stop\n"
+		"		;;\n"
+		"		stop\n"
+		"	restart)\n"
+		"		start\n"
+		"		;;\n"
+		"	status)\n"
+		"		status\n"
+		"		;;\n"
+		"	*)\n"
+		"		echo \"Usage: $NAME (start|stop|restart|status)\" >&2\n"
+		"		exit 1\n"
+		"esac\n"
+		"exit 0\n" << endl;
+	service.close();
+	} else {
+		if (getuid()) {
+			logObj->out(text.errorSystemdRoot, Error);
+		}
+		if (fs::is_directory("/etc/systemd") && fs::is_regular_file(sysService)) {
+			logObj->out("Found an existing systemd service", Warning);
+		}
+		if (fs::is_directory("/etc/systemd") && !fs::is_regular_file(sysService)) {
+			logObj->out("Making systemd service at " + sysService + "...", Info);
+			ofstream service(sysService);
+			service << "[Unit]" << endl << "Description=Starts Hajime" << endl;
+			service << endl << "[Service]\nType=simple\nWorkingDirectory=" << fs::current_path().string() << "\nExecStart=" << fs::current_path().string()  << "/hajime\n\n[Install]\nWantedBy=multi-user.target";
+			service.close();
+		}
+		if (!fs::is_directory("/etc/systemd")) {
+			logObj->out(text.errorNoSystemd, Error);
+		}
 	}
-	if (fs::is_directory("/etc/systemd") && !fs::is_regular_file(sysService)) {
-		logObj->out("Making systemd service at " + sysService + "...", Info);
-		ofstream service(sysService);
-		service << "[Unit]" << endl << "Description=Starts Hajime" << endl;
-		service << endl << "[Service]\nType=simple\nWorkingDirectory=" << fs::current_path().string() << "\nExecStart=" << fs::current_path().string()  << "/hajime\n\n[Install]\nWantedBy=multi-user.target";
-		service.close();
-	}
-	if (!fs::is_directory("/etc/systemd")) {
-		logObj->out(text.errorNoSystemd, Error);
-	}
-	#endif
+		#endif
 }
 
 
