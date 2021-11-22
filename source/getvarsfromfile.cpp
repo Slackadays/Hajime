b1d655c2573a323e1a0d7e8efd2234c5ec4e17d1
Slackadays <69403549+Slackadays@users.noreply.github.com>
2021-11-22 15:28:07 -0800

    Fix file parsing with out-of-order keys

--- source/getvarsfromfile.cpp
+++ source/getvarsfromfile.cpp
@@ -11,31 +11,33 @@ auto continueInFile = [](std::fstream &inFile){return (inFile.good() && !inFile.
 auto checkBadChars = [](auto ch){return (ch != '\0') && (ch != '#') && (ch != '=');};
 
 vector<string> getVarsFromFile(string filename, vector<string> inputVars) {
-        std::fstream file(filename, std::fstream::in); //configuration file open for reading
         vector <string> outputVars;
-        string line = "", key = "", value = "";
-        for (unsigned int i = 0, lineNum = 0; continueInFile(file); lineNum++, i = 0, key = "", value = "") {
-                getline(file, line); //get a line from file and save it to line
-                if (checkBadChars(line[i])) {
-                        for (;checkBadChars(line[i]); i++) { //skips past anything that isn't in a quote
-                          key += line[i]; //append the thing to look for with a known-good character
-                        }
-                        for (i++;(i < line.length()) && (line[i] != '#'); i++) { //increment i by 1 to skip the =
-                                value += line[i];
-                        }
-                        bool success = false;
-                        for (const auto &iteratorVal : inputVars) {
-                                if (key == iteratorVal) {
-                                        outputVars.push_back(value);
-                                        success = true;
-                                }
-                        }
-                        if (!success) {
-                          outputVars.push_back("");
-                        }
-                }
+        for (const auto &iteratorVal : inputVars) { //cycle the whole file for a particular key in inputVars so that they don't have to be in order
+          std::fstream file(filename, std::fstream::in); //configuration file open for reading
+          string line = "", key = "", value = "";
+          bool success = false;
+          for (unsigned int i = 0, lineNum = 0; continueInFile(file); lineNum++, i = 0, key = "", value = "") {
+                  getline(file, line); //get a line from file and save it to line
+                  if (checkBadChars(line[i])) {
+                          for (;checkBadChars(line[i]); i++) { //skips past anything that isn't in a quote
+                            key += line[i]; //append the thing to look for with a known-good character
+                          }
+                          for (i++;(i < line.length()) && (line[i] != '#'); i++) { //increment i by 1 to skip the =
+                            value += line[i];
+                          }
+                          if (key == iteratorVal) {
+                            outputVars.push_back(value);
+                            success = true;
+                          }
+                      }
+              }
+              if (success) {
+                success = false;
+              } else {
+                outputVars.push_back("");
+              }
+              file.close();  //get rid of the file in memory
         }
-        file.close();  //get rid of the file in memory
         return outputVars;
 }
 
