a27912e1ebb5af50bc81f59a15cf0d9148c7db66
Slackadays <69403549+Slackadays@users.noreply.github.com>
2021-12-28 11:31:12 -0500

    Improve color hashing

--- source/output.cpp
+++ source/output.cpp
@@ -7,6 +7,7 @@
 #include <regex>
 #include <functional>
 #include <unordered_map>
+#include <cmath>
 
 using std::string;
 using std::ofstream;
@@ -187,74 +188,112 @@ void Output::addServerName(string serverName) {
 
 string Output::getColorByID() {
 	std::hash<string> hasher;
-	int colorID;
+	int selection;
 	if (threadToNameMap.count(std::this_thread::get_id())) { //do not access threadtonamemap directly because doing so would add the thread id as a key
-		colorID = hasher(threadToNameMap[std::this_thread::get_id()]) % 29;
+		selection = hasher(threadToNameMap[std::this_thread::get_id()]) % 211;
 	} else if (std::this_thread::get_id() == main_thread) {
-		colorID = 13;
+		return "\033[36m";
 	} else {
-		colorID = 0;
+		return "";
 	}
-	switch (colorID) {
+	float dummy;
+ 	float bgID = std::modf(selection / 15.0, &dummy);
+	string returnedColor;
+	switch ((int)ceil(selection / 14.0)) {
 		case 1:
-			return "\033[31m"; //dark red text
+			returnedColor += "\033[31m"; //dark red text
+			break;
 		case 2:
-			return "\033[32m"; //dark green text
+			returnedColor += "\033[32m"; //dark green text
+			break;
 		case 3:
-			return "\033[33m"; //dark yellow text
+			returnedColor += "\033[33m"; //dark yellow text
+			break;
 		case 4:
-			return "\033[34m"; //dark blue text
+			returnedColor += "\033[34m"; //dark blue text
+			break;
 		case 5:
-			return "\033[35m"; //dark magenta text
+			returnedColor += "\033[35m"; //dark magenta text
+			break;
 		case 6:
-			return "\033[36m"; //dark cyan text
+			returnedColor += "\033[36m"; //dark cyan text
+			break;
 		case 7:
-			return "\033[37m"; //dark white text
+			returnedColor += "\033[37m"; //dark white text
+			break;
 		case 8:
-			return "\033[90m"; //bright black text
+			returnedColor += "\033[90m"; //bright black text
+			break;
 		case 9:
-			return "\033[91m"; //bright red text
+			returnedColor += "\033[91m"; //bright red text
+			break;
 		case 10:
-			return "\033[92m"; //bright green text
+			returnedColor += "\033[92m"; //bright green text
+			break;
 		case 11:
-			return "\033[93m"; //bright yellow text
+			returnedColor += "\033[93m"; //bright yellow text
+			break;
 		case 12:
-			return "\033[94m"; //bright blue text
+			returnedColor += "\033[94m"; //bright blue text
+			break;
 		case 13:
-			return "\033[95m"; //bright magenta text
+			returnedColor += "\033[95m"; //bright magenta text
+			break;
 		case 14:
-			return "\033[96m"; //bright cyan text
-		case 15:
-			return "\033[41m"; //dark red background
-		case 16:
-			return "\033[42m"; //gark green bg
-		case 17:
-			return "\033[43m"; //dy bg
-		case 18:
-			return "\033[44m"; //db bg
-		case 19:
-			return "\033[45m"; //dm bg
-		case 20:
-			return "\033[46m"; //dc bg
-		case 21:
-			return "\033[47m"; //dw bg
-		case 22:
-			return "\033[100m"; //bb bg
-		case 23:
-			return "\033[101m"; //br bg
-		case 24:
-			return "\033[102m"; //bg bg
-		case 25:
-			return "\033[103m"; //by bg
-		case 26:
-			return "\033[104m"; //bb bg
-		case 27:
-			return "\033[105m"; //bm bg
-		case 28:
-			return "\033[106m"; //bc bg
+			returnedColor += "\033[96m"; //bright cyan text
+			break;
 		default:
-			return "";
+			break;
+		}
+		switch ((int)round(bgID * 15.0)) {
+		case 0:
+			returnedColor += "\033[41m"; //dark red background
+			break;
+		case 1:
+			returnedColor += "\033[42m"; //gark green bg
+			break;
+		case 2:
+			returnedColor += "\033[43m"; //dy bg
+			break;
+		case 3:
+			returnedColor += "\033[44m"; //db bg
+			break;
+		case 4:
+			returnedColor += "\033[45m"; //dm bg
+			break;
+		case 5:
+			returnedColor += "\033[46m"; //dc bg
+			break;
+		case 6:
+			returnedColor += "\033[47m"; //dw bg
+			break;
+		case 7:
+			returnedColor += "\033[100m"; //bb bg
+			break;
+		case 8:
+			returnedColor += "\033[101m"; //br bg
+			break;
+		case 9:
+			returnedColor += "\033[102m"; //bg bg
+			break;
+		case 10:
+			returnedColor += "\033[103m"; //by bg
+			break;
+		case 11:
+			returnedColor += "\033[104m"; //bb bg
+			break;
+		case 12:
+			returnedColor += "\033[105m"; //bm bg
+			break;
+		case 13:
+			returnedColor += "\033[106m"; //bc bg
+			break;
+		case 14:
+			break;
+		default:
+			break;
 	}
+	return returnedColor;
 }
 
 std::shared_ptr<Output> logObj = std::make_shared<Output>();
