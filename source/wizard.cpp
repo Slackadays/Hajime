0d95037ee6833af11f6e741967a8099f2c298185
Slackadays <69403549+Slackadays@users.noreply.github.com>
2021-12-28 16:59:14 -0500

    Improve spinner

--- source/wizard.cpp
+++ source/wizard.cpp
@@ -2,7 +2,6 @@
 #include <filesystem>
 #include <vector>
 #include <random>
-#include <thread>
 
 #include "output.hpp"
 #include "installer.hpp"
@@ -14,23 +13,27 @@ void Wizard::pause(float mean, float stdev) {
 	if (doArtificialPauses) {
 		std::random_device rand;
 		std::normal_distribution<float> normal(mean, stdev);
-		bool spinnerRunning = true;
-		std::thread spinner([&spinnerRunning](){
-			while (spinnerRunning) {
-				std::cout << "\b|" << std::flush;
-				std::this_thread::sleep_for(std::chrono::milliseconds(100));
-				std::cout << "\b/" << std::flush;
-				std::this_thread::sleep_for(std::chrono::milliseconds(100));
-				std::cout << "\b-" << std::flush;
-				std::this_thread::sleep_for(std::chrono::milliseconds(100));
-				std::cout << "\b\\" << std::flush;
-				std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		int target = (int)abs(normal(rand));
+		for (int ms = 0; ms < target; ms += 100) {
+			std::cout << "\b|" << std::flush;
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			if (ms += 100; ms >= target) {
+				break;
 			}
-			std::cout << "\b" << std::flush;
-		});
-		std::this_thread::sleep_for(std::chrono::milliseconds((int)abs(normal(rand))));
-		spinnerRunning = false;
-		spinner.join();
+			std::cout << "\b/" << std::flush;
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			if (ms += 100; ms >= target) {
+				break;
+			}
+			std::cout << "\b―" << std::flush; //horizontal bar character
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			if (ms += 100; ms >= target) {
+				break;
+			}
+			std::cout << "\b\\" << std::flush;
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		}
+		std::cout << "\b" << std::flush;
 	}
 }
 
