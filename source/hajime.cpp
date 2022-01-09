0cff906ee5c9a0db10dc7837a773825af60c68dd
Slackadays <69403549+Slackadays@users.noreply.github.com>
2022-01-09 15:30:57 -0500

    Try again

--- source/hajime.cpp
+++ source/hajime.cpp
@@ -38,6 +38,9 @@ using std::shared_ptr;
 using std::make_shared;
 using std::vector;
 
+bool ee = false;
+vector<Server> serverVec; //create an array of individual server objects
+vector<std::jthread> threadVec; //create an array of thread objects
 string defaultServerConfFile = "MyServer.conf";
 string defaultServersFile = "servers.conf";
 string sysdService = "/etc/systemd/system/hajime.service"; //systemd service file location
@@ -47,6 +50,7 @@ string hajConfFile = "";
 bool readSettings();
 void dividerLine();
 vector<string> toVec(string input);
+void processHajimeCommand(vector<string> input);
 
 int main(int argc, char *argv[]) {
 	atexit(dividerLine);
@@ -55,18 +59,18 @@ int main(int argc, char *argv[]) {
 	DWORD dwMode = 0;
 	GetConsoleMode(hOut, &dwMode);
 	if (!SetConsoleMode(hOut, (dwMode += ENABLE_VIRTUAL_TERMINAL_PROCESSING))) {
-		logObj->noColors = true;
+		hjlog->noColors = true;
 	}
 	SetConsoleOutputCP(CP_UTF8); //fix broken accents on Windows
 	#endif
 	dividerLine();
 	for (int i = 1; i < argc; i++) { //search for the help flag first
 		auto flag = [&i, &argv](auto ...fs){return (!strcmp(fs, argv[i]) || ...);}; //compare flags with a parameter pack pattern
-		auto helpOut = [](auto ...num){(logObj->out(text.help[num]), ...);}; //print multiple strings pointed to by text.help[] at once by using a parameter pack
+		auto helpOut = [](auto ...num){(hjlog->out(text.help[num]), ...);}; //print multiple strings pointed to by text.help[] at once by using a parameter pack
 		auto assignNextToVar = [&argc, &argv, &i](auto &var){if (i == (argc - 1)) {return false;} else {var = argv[(i + 1)]; i++; return true;}};
 		if (flag("-h", "--help")) { //-h = --help = help
 			helpOut(0, 1);
-			logObj->out(text.help[2] + (string)argv[0] + text.help[3]); //show example of hajime and include its executed file
+			hjlog->out(text.help[2] + (string)argv[0] + text.help[3]); //show example of hajime and include its executed file
 			helpOut(4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19); //note: Linux doesn't put an endline at the end upon exit, but Windows does
 			return 0; //if someone is asking for help, ignore any other flags and just display the help screen
 		}
@@ -74,7 +78,7 @@ int main(int argc, char *argv[]) {
 			if ((i < (argc - 1)) && string(argv[i + 1]).front() != '-') {
 				text.applyLang(argv[i + 1]);
 			} else {
-				logObj->out(text.errorNotEnoughArgs, Error);
+				hjlog->out(text.errorNotEnoughArgs, Error);
 				return 0;
 			}
 		}
@@ -84,13 +88,13 @@ int main(int argc, char *argv[]) {
 		auto assignNextToVar = [&argc, &argv, &i](auto &var){if (i == (argc - 1)) {return false;} else {var = argv[(i + 1)]; i++; return true;}}; //tries to assign the next argv argument to some variable; if it is not valid, then return an error
 		if (flag("-f", "--server-file")) {
 			if (!assignNextToVar(defaultServerConfFile)) {
-				logObj->out(text.errorNotEnoughArgs, Error);
+				hjlog->out(text.errorNotEnoughArgs, Error);
 				return 0;
 			}
 		}
 		if (flag("-hf", "--hajime-file")) {
 			if (!assignNextToVar(hajDefaultConfFile)) {
-				logObj->out(text.errorNotEnoughArgs, Error);
+				hjlog->out(text.errorNotEnoughArgs, Error);
 				return 0;
 			}
 		}
@@ -114,13 +118,16 @@ int main(int argc, char *argv[]) {
 			return 0;
 		}
 		if (flag("-v", "--verbose")) {
-			logObj->verbose = true;
+			hjlog->verbose = true;
 		}
 		if (flag("-m", "--monochrome", "--no-colors")) {
-			logObj->noColors = true;
+			hjlog->noColors = true;
 		}
 		if (flag("-d", "--debug")) {
-			logObj->debug = true;
+			hjlog->debug = true;
+		}
+		if (flag("-ee")) {
+			ee = true;
 		}
 		if (flag("-i", "--install-hajime")) {
 			wizard.initialHajimeSetup(hajDefaultConfFile, defaultServersFile, defaultServerConfFile, sysdService);
@@ -130,68 +137,48 @@ int main(int argc, char *argv[]) {
 			wizard.doArtificialPauses = false;
 		}
 		if (flag("-tc", "--thread-colors")) {
-			logObj->showThreadsAsColors = true;
+			hjlog->showThreadsAsColors = true;
 		}
 		if (flag("-it", "--show-info-type")) {
-			logObj->showExplicitInfoType = true;
+			hjlog->showExplicitInfoType = true;
 		}
 	}
 	if (fs::is_regular_file(hajDefaultConfFile)) {
 		readSettings();
-		empty(logFile) ? logObj->out(text.infoNoLogFile, Info) : logObj->init(logFile);
+		empty(logFile) ? hjlog->out(text.infoNoLogFile, Info) : hjlog->init(logFile);
 	} else {
-		logObj->out(text.errorConfDoesNotExist1 + hajDefaultConfFile + text.errorConfDoesNotExist2, Error);
-		logObj->out(text.questionDoSetupInstaller, Question);
-		switch (logObj->getYN(text.optionAttendedInstallation, text.optionUnattendedInstallation, text.optionSkipSetup)) {
+		hjlog->out(text.errorConfDoesNotExist1 + hajDefaultConfFile + text.errorConfDoesNotExist2, Error);
+		hjlog->out(text.questionDoSetupInstaller, Question);
+		switch (hjlog->getYN(text.optionAttendedInstallation, text.optionUnattendedInstallation, text.optionSkipSetup)) {
 			case 1:
 				dividerLine();
 				wizard.initialHajimeSetup(hajDefaultConfFile, defaultServersFile, defaultServerConfFile, sysdService);
-				logObj->out(text.questionStartHajime, Question);
-				if (!logObj->getYN()) {
+				hjlog->out(text.questionStartHajime, Question);
+				if (!hjlog->getYN()) {
 					return 0;
 				}
 				break;
 			case 2:
-				logObj->out(text.errorOptionNotAvailable, Error);
+				hjlog->out(text.errorOptionNotAvailable, Error);
 				break;
 			case 3:
 				return 0;
 		}
 	}
 	if (!fs::is_regular_file(defaultServersFile)) {
-		logObj->out(text.errorNoServersFile, Error);
+		hjlog->out(text.errorNoServersFile, Error);
 		return 0;
 	}
-	vector<Server> serverVec; //create an array of individual server objects
-	vector<std::jthread> threadVec; //create an array of thread objects
-	Server server(logObj); //create a template object
+	Server server(hjlog); //create a template object
 	for (const auto &serverIt : getVarsFromFile(defaultServersFile)) { //loop through all the server files found
 		serverVec.push_back(server); //add a copy of server to use
 		threadVec.push_back(std::jthread(&Server::startServer, serverVec.back(), serverIt)); //add a thread that links to startServer and is of the last server object added, use serverIt as parameter
 	}
 	while(true) { //future command processing
-		logObj->out(text.infoEnterCommand, Info);
+		hjlog->out(text.infoEnterCommand, Info);
 		string command = "";
 		std::getline(std::cin, command);
-		vector<string> commandVec = toVec(command);
-		if (commandVec[0] == "term" || commandVec[0] == "t") {
-			if (commandVec.size() >= 2) {
-				try {
-					if (stoi(commandVec[1]) > serverVec.size() || stoi(commandVec[1]) < 1) {
-						logObj->out(text.errorInvalidServerNumber, Error);
-					} else {
-						serverVec[stoi(commandVec[1]) - 1].terminalAccessWrapper();
-					}
-				} catch (...) {
-					logObj->out(text.errorServerSelectionInvalid, Error);
-				}
-			} else {
-				logObj->out(text.errorNotEnoughArgs, Error);
-			}
-		} else {
-			logObj->out(text.errorInvalidCommand, Error);
-			logObj->out(text.errorInvalidHajCommand1, Error);
-		}
+		processHajimeCommand(toVec(command));
 	}
 	return 0;
 }
@@ -199,21 +186,21 @@ int main(int argc, char *argv[]) {
 bool readSettings() {
 	vector<string> settings{"serversfile", "defserverconf", "logfile", "systemdlocation", "debug"};
 	if (!fs::is_regular_file(hajDefaultConfFile)) {
-		logObj->out(text.debugHajDefConfNoExist1 + hajDefaultConfFile + text.debugHajDefConfNoExist2, Debug);
+		hjlog->out(text.debugHajDefConfNoExist1 + hajDefaultConfFile + text.debugHajDefConfNoExist2, Debug);
 		return 0;
 	}
 	vector<string> results = getVarsFromFile(hajDefaultConfFile, settings);
 	for (vector<string>::iterator firstSetIterator = settings.begin(), secondSetIterator = results.begin(); firstSetIterator != settings.end() && secondSetIterator != results.end(); ++firstSetIterator, ++secondSetIterator) {
 		auto setVar = [&](string name, string& tempVar){if (*firstSetIterator == name) {tempVar = *secondSetIterator;}};
 		auto setVari = [&](string name, int& tempVar){if (*firstSetIterator == name) {try {tempVar = stoi(*secondSetIterator);} catch(...) {tempVar = 0;}}};
-		logObj->out(text.debugReadingReadsettings, Debug);
+		hjlog->out(text.debugReadingReadsettings, Debug);
 		setVar(settings[0], defaultServersFile);
 		setVar(settings[1], defaultServerConfFile);
 		setVar(settings[2], logFile);
 		setVar(settings[3], sysdService);
-		setVari(settings[4], logObj->debug);
+		setVari(settings[4], hjlog->debug);
 	}
-	logObj->out(text.debugReadReadsettings + hajDefaultConfFile, Debug);
+	hjlog->out(text.debugReadReadsettings + hajDefaultConfFile, Debug);
 	return 1;
 }
 
@@ -222,13 +209,13 @@ void dividerLine() {
 	CONSOLE_SCREEN_BUFFER_INFO w;
 	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &w);
 	for (int i = 0; i < w.dwSize.X; i++) {
-		logObj->out("─", None, 0, 0);
+		hjlog->out("─", None, 0, 0);
 	}
 	#else
 	struct winsize w;
 	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 	for (int i = 0; i < w.ws_col; i++) {
-		logObj->out("─", None, 0, 0);
+		hjlog->out("─", None, 0, 0);
 	}
 	#endif
 	std::cout << std::endl;
@@ -251,4 +238,29 @@ vector<string> toVec(string input) {
 		output.push_back(temp); //add the finished flag to the vector of flags
 	}
 	return output;
+}
+
+void processHajimeCommand(vector<string> input) {
+	if (input[0] == "term" || input[0] == "t") {
+		if (input.size() >= 2) {
+			try {
+				if (stoi(input[1]) > input.size() || stoi(input[1]) < 1) {
+					hjlog->out(text.errorInvalidServerNumber, Error);
+				} else {
+					serverVec[stoi(input[1]) - 1].terminalAccessWrapper();
+				}
+			} catch (...) {
+				hjlog->out(text.errorServerSelectionInvalid, Error);
+			}
+		} else {
+			hjlog->out(text.errorNotEnoughArgs, Error);
+		}
+	} else if (input[0] == "ee" && ee && text.language == "en") {
+		hjlog->out("https://www.youtube.com/watch?v=ccY25Cb3im0");
+	} else if (input[0] == "ee" && ee && text.language == "es") {
+		hjlog->out("https://www.youtube.com/watch?v=iFClTRUnmKc");
+	} else {
+		hjlog->out(text.errorInvalidCommand, Error);
+		hjlog->out(text.errorInvalidHajCommand1, Error);
+	}
 }
