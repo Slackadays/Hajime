6fb2598da26607a0f0f74c1baeeb0b6365b3aa04
Slackadays <69403549+Slackadays@users.noreply.github.com>
2022-02-04 17:47:42 -0500

    Another fix

--- source/server.hpp
+++ source/server.hpp
@@ -40,7 +40,7 @@ using std::vector;
 using std::cout;
 
 class Server {
-	bool hasOutput = false, hasOutputUSB = false, hasMounted = false;
+	bool hasOutputUSB = false, hasMounted = false;
 
 	int systemi = 0;
 
