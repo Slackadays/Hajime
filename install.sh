86455c437a41c6beea357a1876cd234c2edbf3f6
Slackadays <69403549+Slackadays@users.noreply.github.com>
2022-02-04 16:25:12 -0500

    Add capability to install script, improve

--- install.sh
+++ install.sh
@@ -1,99 +1,99 @@
 #!/bin/sh
-echo "Installing necessary packages..."
-if [ "$USER" != "root" ]
-then
-        echo "You may need to be the root user to install g++ and git."
-fi
-if [ "$OSTYPE" = "FreeBSD" ]
-then
-        echo "FreeBSD detected"
-        pkg update
-        pkg install gcc10 git
-        pkg upgrade
-fi
+
+run_and_add_perms() {
+  TARGET_FILE=$1
+  unzip $1
+  if [ -e hajime ]
+  then
+    echo "Assigning the CAP_PERFMON capability to Hajime..."
+    sudo setcap 'cap_perfmon+pe' hajime
+    chmod +x hajime
+    rm $1
+    ./hajime
+    exit 0
+  fi
+}
+
+update_and_install() {
+  echo "Installing necessary packages..."
+  if [ "$USER" != "root" ]
+  then
+          echo "You may need to be the root user to install g++, git, and cmake."
+  fi
+  if [ "$OSTYPE" = "FreeBSD" ]
+  then
+          echo "FreeBSD detected"
+          pkg update
+          pkg install gcc10 git cmake
+          pkg upgrade
+  fi
+  if [ "$OSTYPE" = "OpenBSD" ]
+  then
+  	echo "OpenBSD detected"
+  	pkg_add git cmake
+  fi
+  if [ "$(uname -s)" = "Linux" ]
+  then
+    sudo apt update
+    sudo apt install -y g++10 cmake git
+    sudo yum install -y clang && sudo yum install -y git
+    sudo yum install -y cmake
+  fi
+}
+
 if [ "$(uname -s)" = "Darwin" ]
 then
 	echo "macOS detected"
 	if [ "$(uname -m)" = "arm64" ]
 	then
 		echo "Downloading Hajime for macOS ARM directly"
-		curl -o hajime-macos-arm64.zip -L https://github.com/Slackadays/Hajime/releases/latest/download/hajime-macos-arm64.zip
-		unzip hajime-macos-arm64.zip
-                if [ -e hajime ]
-                then
-                        chmod +x hajime
-			rm hajime-macos-arm64.zip
-                        ./hajime
-                        exit 0
-                fi
+		curl -s -o hajime-macos-arm64.zip -L https://github.com/Slackadays/Hajime/releases/latest/download/hajime-macos-arm64.zip
+		run_and_add_perms hajime-macos-arm64.zip
 	fi
   if [ "$(uname -m)" = "x86_64" ]
 	then
 		echo "Downloading Hajime for macOS x86_64 directly"
-		curl -o hajime-macos-amd64.zip -L https://github.com/Slackadays/Hajime/releases/latest/download/hajime-macos-amd64.zip
-		unzip hajime-macos-amd64.zip
-                if [ -e hajime ]
-                then
-                        chmod +x hajime
-			rm hajime-macos-amd64.zip
-                        ./hajime
-                        exit 0
-                fi
+		curl -s -o hajime-macos-amd64.zip -L https://github.com/Slackadays/Hajime/releases/latest/download/hajime-macos-amd64.zip
+		run_and_add_perms hajime-macos-amd64.zip
 	fi
+  echo "Can't find a supported platform; compiling Hajime now"
+  update_and_install
 fi
-if [ "$(uname -s)" = "Linux" ]
+if [ "$(uname -s)" = "Linu" ]
 then
 	echo "Linux detected"
+  echo "Updating and installing the unzip package..."
+  sudo apt update
+  sudo apt install -y unzip
+  sudo yum install -y unzip
 	if [ "$(uname -m)" = "x86_64" ]
 	then
 		echo "Trying to download Hajime for Linux amd64 directly"
-		curl -o hajime-linux-amd64.zip -L https://github.com/Slackadays/Hajime/releases/latest/download/hajime-linux-amd64.zip
-		unzip hajime-linux-amd64.zip
-		if [ -e hajime ]
-		then
-			chmod +x hajime
-			rm hajime-linux-amd64.zip
-			./hajime
-			exit 0
-		fi
+		curl -s -o hajime-linux-amd64.zip -L https://github.com/Slackadays/Hajime/releases/latest/download/hajime-linux-amd64.zip
+		run_and_add_perms hajime-linux-amd64.zip
 	fi
 	if [ -n "$(uname -m | grep armv7l)" ] || [ -n "$(uname -m | grep aarch64)" ]
 	then
 		echo "Trying to download Hajime for Linux arm32hf directly"
-		curl -o hajime-linux-arm32.zip -L https://github.com/Slackadays/Hajime/releases/latest/download/hajime-linux-arm32.zip
-		unzip hajime-linux-arm32.zip
-		if [ -e hajime ]
-		then
-			chmod +x hajime
-			rm hajime-linux-arm32.zip
-			./hajime
-			exit 0
-		fi
+		curl -s -o hajime-linux-arm32hf.zip -L https://github.com/Slackadays/Hajime/releases/latest/download/hajime-linux-arm32hf.zip
+		run_and_add_perms hajime-linux-arm32hf.zip
 	fi
-	echo "Compiling Hajime now"
-	sudo apt update && sudo apt -y install g++ git #g++-10 or the otherwise latest version of g++ that supports c++17
-	sudo apt -y upgrade
-	sudo yum install -y clang
-	sudo yum install -y git
+	echo "Can't find a supported platform; compiling Hajime now"
+  update_and_install
 fi
-if [ "$OSTYPE" = "OpenBSD" ]
-then
-	echo "OpenBSD detected"
-	pkg_add git
-fi
-echo "Downloading..."
-git clone https://github.com/slackadays/hajime
+echo "Cloning repo..."
+git clone https://github.com/slackadays/Hajime
 echo "Compiling..."
 echo "This may take from a few seconds to a few minutes depending on your system speed."
-cd hajime
-if [ "$OSTYPE" = "OpenBSD" ]
-then
-	sed -i s/g++/eg++/g fakemake
-fi
-sh fakemake
+cd Hajime
+cmake source
+cmake --build . -j 9
 echo "Cleaning up..."
-chown -R $USER ../../Hajime/.git #change perms for certain misbehaving files that come with git
-mv hajime ../../ #move the binary to the original folder where the script was started
-rm -rf ../../Hajime #remove the cloned directory, ignore junk files from .git
-cd ../../
+chown -R $USER "../Hajime/.git" #change perms for certain misbehaving files that come with git
+mv hajime ../ #move the binary to the original folder where the script was started
+rm -rf ../Hajime #remove the cloned directory, ignore junk files from .git
+cd ../
+echo "Assigning the CAP_PERFMON capability to Hajime..."
+sudo setcap 'cap_perfmon+pe' hajime
 ./hajime
+exit 0
