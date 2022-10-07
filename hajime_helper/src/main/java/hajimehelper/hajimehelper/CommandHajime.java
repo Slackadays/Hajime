2bcf73d38bed6391500c6fcd036f308e2de4df97
Slackadays <lightingdzeyenr@gmail.com>
2022-10-06 21:54:49 -0400

    IntelliJ is broken

--- hajime_helper/src/main/java/hajimehelper/hajimehelper/CommandHajime.java
+++ hajime_helper/src/main/java/hajimehelper/hajimehelper/CommandHajime.java
@@ -1,2 +1,18 @@
-package hajimehelper.hajimehelper;public class CommandHajime {
+package hajimehelper.hajimehelper;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
+
+public class CommandHajime implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            player.sendMessage("Hello World!");
+        }
+        return true;
+    }
 }
--- hajime_helper/src/main/java/hajimehelper/hajimehelper/HajimeHelper.java
+++ hajime_helper/src/main/java/hajimehelper/hajimehelper/HajimeHelper.java
@@ -1,13 +1,13 @@
 package hajimehelper.hajimehelper;
 
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HajimeHelper extends JavaPlugin {
-
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getCommand("hajime").setExecutor(new CommandHajime());
     }
 
     @Override
