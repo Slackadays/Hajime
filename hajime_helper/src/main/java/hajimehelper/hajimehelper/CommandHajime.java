995aa2c9ea7c9c8b3045cdee66249f2f1fbeca1d
Slackadays <lightingdzeyenr@gmail.com>
2022-10-06 21:52:10 -0400

    Add initial shim

---
+++ hajime_helper/src/main/java/hajimehelper/hajimehelper/CommandHajime.java
@@ -0,0 +1,2 @@
+package hajimehelper.hajimehelper;public class CommandHajime {
+}
---
+++ hajime_helper/src/main/java/hajimehelper/hajimehelper/HajimeHelper.java
@@ -0,0 +1,17 @@
+package hajimehelper.hajimehelper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HajimeHelper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
