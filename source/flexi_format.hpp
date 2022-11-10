6dcf82678b1ebe0976ffdfaed21a19de28659669
Slackadays <lightingdzeyenr@gmail.com>
2022-11-09 19:42:17 -0500

    Fix macOS?

--- source/flexi_format.hpp
+++ source/flexi_format.hpp
@@ -6,5 +6,9 @@
 #include <string>
 
 std::string flexi_format(const auto& format, const auto&... args) {
+    #if defined(__APPLE__)
+    return fmt::vformat(fmt::detail::to_string_view(format), fmt::make_format_args(args...));
+    #else
     return fmt::vformat(fmt::to_string_view(format), fmt::make_format_args(args...));
+    #endif
 }
